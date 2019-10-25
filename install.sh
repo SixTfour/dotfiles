9eadf3fdedcad79d784bc43386c43aeb3bed34fe
SixTfour <sheldonklaus14@gmail.com>
2019-10-25 13:09:43 -0500

    Added oh-my-zsh stuff

---
+++ install.sh
@@ -0,0 +1,13 @@
+  
+#!/bin/bash
+mkdir ~/code
+# Install OhMyZsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# Install Powerlevel 10k theme
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+# Install all Homebrew packages
+sh ./brew.sh
+# Link zsh/.zshrc to ~/.zshrc
+sh ./link.sh
+
+echo "Finished: Environment successfully installed!"
---
+++ link.sh
@@ -0,0 +1,4 @@
+# Link .zshrc
+ln -s ~/scripts/zsh/.zshrc ~/.zshrc
+# Copy to VS Code user settings.json
+cp -f ./vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
